e2d6f43c96ad9f18ca2e184156b814f52afae0e6
Yammy33 <luke.meier1@gmail.com>
2024-01-18 20:33:20 +0100

    add cardsets edit view

--- app/src/main/java/com/example/myapplication/activty/CardsetEditViewActivity.java
+++ app/src/main/java/com/example/myapplication/activty/CardsetEditViewActivity.java
@@ -5,27 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import com.example.myapplication.R;
 
-public class CardsetViewActivity extends AppCompatActivity {
-
-    LinearLayout cardsets;
+public class CardsetEditViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cardset_view);
-        cardsets = findViewById(R.id.cards);
+        setContentView(R.layout.activity_cardset_edit_view);
     }
 
     public void add(View view) {
-        Intent intent = new Intent(this, CardsetEditViewActivity.class);
-        startActivity(intent);
     }
     public void goBack(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, CardsetViewActivity.class);
         startActivity(intent);
     }
 }
